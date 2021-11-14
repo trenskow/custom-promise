b072c52b52094b9e746f08d2c9129bd452f60d10
Kristian Trenskow <trenskow@me.com>
2021-11-14 07:49:55 +0100

    Added support for finally.

--- index.js
+++ index.js
@@ -5,37 +5,47 @@ class CustomPromise {
 	constructor() {
 		this._state = 'pending';
 		this._awaiters = [];
+		this._finalizers = [];
 	}
 
 	_resolve(result) {
 		if (this._state !== 'pending') return;
 		this._state = 'fulfilled';
 		this._result = result;
-		this._awaiters.splice(0).forEach(([fulfilled]) => fulfilled(result));
+		this._awaiters.splice(0).forEach(([onFulfilled]) => onFulfilled(result));
+		this._finalizers.splice(0).forEach((onFinally) => onFinally());
 	}
 
 	_reject(error) {
 		if (this._state !== 'pending') return;
 		this._state = 'rejected';
 		this._error = error;
-		this._awaiters.splice(0).forEach(([, rejected]) => rejected(error));
+		this._awaiters.splice(0).forEach(([, onRejected]) => onRejected(error));
+		this._finalizers.splice(0).forEach((onFinally) => onFinally());
 	}
 
-	then(fulfilled, rejected) {
+	then(onFulfilled, onRejected) {
 
-		if (fulfilled && typeof fulfilled !== 'function') throw new TypeError('fulfilled must be a function.');
-		if (rejected && typeof rejected !== 'function') throw new TypeError('rejected must be a function.');
+		if (onFulfilled && typeof onFulfilled !== 'function') throw new TypeError('fulfilled must be a function.');
+		if (onRejected && typeof onRejected !== 'function') throw new TypeError('rejected must be a function.');
 
 		if (this._state === 'fulfilled') {
-			if (fulfilled) fulfilled(this._result);
+			if (onFulfilled) onFulfilled(this._result);
 		}
 		else if (this._state === 'rejected') {
-			if (rejected) rejected(this._error);
+			if (onRejected) onRejected(this._error);
 		}
 		else {
-			this._awaiters.push([fulfilled, rejected]);
+			this._awaiters.push([onFulfilled, onRejected]);
 		}
 		
+	}
+
+	finally(onFinally) {
+		if (this._state !== 'pending') onFinally();
+		else {
+			this._finalizers.push(onFinally);
+		}
 	}
 
 }
