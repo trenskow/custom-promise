010e75acc29900e0cd9e84d4b15e6a32398a2544
Kristian Trenskow <trenskow@me.com>
2024-03-25 09:21:05 +0100

    `then` now returns a new promise.

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ class CustomPromise {
 
 	constructor() {
 		this._state = 'pending';
-		this._awaiters = [];
+		this._result = undefined;
+		this._resolveCallback = undefined;
+		this._rejectCallback = undefined;
 		this._finalizers = [];
-	}
-
-	_reset() {
-		this._state = 'pending';
 	}
 
 	_resolve(result) {
 		if (this._state !== 'pending') return;
 		this._state = 'fulfilled';
 		this._result = result;
-		this._awaiters.splice(0).forEach(([onFulfilled]) => onFulfilled(result));
+		this._resolveCallback?.(result);
 		this._finalizers.splice(0).forEach((onFinally) => onFinally());
 	}
 
@@ -24,25 +22,63 @@ class CustomPromise {
 		if (this._state !== 'pending') return;
 		this._state = 'rejected';
 		this._error = error;
-		this._awaiters.splice(0).forEach(([, onRejected]) => onRejected(error));
+		this._rejectCallback?.(error);
 		this._finalizers.splice(0).forEach((onFinally) => onFinally());
 	}
 
 	then(onFulfilled, onRejected) {
+		return new Promise((resolve, reject) => {
 
-		if (onFulfilled && typeof onFulfilled !== 'function') throw new TypeError('fulfilled must be a function.');
-		if (onRejected && typeof onRejected !== 'function') throw new TypeError('rejected must be a function.');
+			const resolveWrapper = (value) => {
+				try {
 
-		if (this._state === 'fulfilled') {
-			if (onFulfilled) onFulfilled(this._result);
-		}
-		else if (this._state === 'rejected') {
-			if (onRejected) onRejected(this._error);
-		}
-		else {
-			this._awaiters.push([onFulfilled, onRejected]);
-		}
-		
+					if (typeof onFulfilled !== 'function') {
+						return resolve(value);
+					}
+
+					const result = onFulfilled(value);
+
+					if (typeof result?.then === 'function') {
+						return result.then(resolve, reject);
+					}
+
+					resolve(result);
+
+				} catch (error) {
+					reject(error);
+				}
+			};
+
+			const rejectedWrapper = (error) => {
+				try {
+
+					if (typeof onRejected !== 'function') {
+						return reject(error);
+					}
+
+					const result = onRejected(error);
+
+					if (typeof result?.then === 'function') {
+						return result.then(resolve, reject);
+					}
+
+					reject(result);
+
+				} catch (error) {
+					reject(error);
+				}
+			};
+
+			if (this._state === 'fulfilled') {
+				resolveWrapper(this._result);
+			} else if (this._state === 'rejected') {
+				rejectedWrapper(this._error);
+			} else {
+				this._resolveCallback = resolveWrapper;
+				this._rejectCallback = rejectedWrapper;
+			}
+
+		});
 	}
 
 	finally(onFinally) {
@@ -50,6 +86,7 @@ class CustomPromise {
 		else {
 			this._finalizers.push(onFinally);
 		}
+		return this;
 	}
 
 }
