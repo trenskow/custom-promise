62d35cd071dbdb881f682ddbc6cca08632236f49
Kristian Trenskow <trenskow@me.com>
2024-08-18 13:40:49 +0200

    More bare metal implementation.

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ class CustomPromise {
 	constructor() {
 		this._state = 'pending';
 		this._result = undefined;
-		this._resolveCallback = undefined;
-		this._rejectCallback = undefined;
 		this._finalizers = [];
+		this._onFulfilled = [];
+		this._onRejected = [];
 	}
 
 	_resolve(result) {
 		if (this._state !== 'pending') return;
 		this._state = 'fulfilled';
 		this._result = result;
-		this._resolveCallback?.(result);
+		this._onFulfilled
+			.filter((onFulfilled) => typeof onFulfilled === 'function')
+			.forEach((onFulfilled) => onFulfilled(result));
 		this._finalizers.splice(0).forEach((onFinally) => onFinally());
 	}
 
@@ -22,63 +24,25 @@ class CustomPromise {
 		if (this._state !== 'pending') return;
 		this._state = 'rejected';
 		this._error = error;
-		this._rejectCallback?.(error);
+		this._onRejected
+			.filter((onRejected) => typeof onRejected === 'function')
+			.forEach((onRejected) => onRejected(error));
 		this._finalizers.splice(0).forEach((onFinally) => onFinally());
 	}
 
 	then(onFulfilled, onRejected) {
-		return new Promise((resolve, reject) => {
 
-			const resolveWrapper = (value) => {
-				try {
+		if (this._state === 'fulfilled') {
+			onFulfilled?.(this._result);
+		} else if (this._state === 'rejected') {
+			onRejected?.(this._error);
+		} else {
+			this._onFulfilled.push(onFulfilled);
+			this._onRejected.push(onRejected);
+		}
 
-					if (typeof onFulfilled !== 'function') {
-						return resolve(value);
-					}
+		return this;
 
-					const result = onFulfilled(value);
-
-					if (typeof result?.then === 'function') {
-						return result.then(resolve, reject);
-					}
-
-					resolve(result);
-
-				} catch (error) {
-					reject(error);
-				}
-			};
-
-			const rejectedWrapper = (error) => {
-				try {
-
-					if (typeof onRejected !== 'function') {
-						return reject(error);
-					}
-
-					const result = onRejected(error);
-
-					if (typeof result?.then === 'function') {
-						return result.then(resolve, reject);
-					}
-
-					reject(result);
-
-				} catch (error) {
-					reject(error);
-				}
-			};
-
-			if (this._state === 'fulfilled') {
-				resolveWrapper(this._result);
-			} else if (this._state === 'rejected') {
-				rejectedWrapper(this._error);
-			} else {
-				this._resolveCallback = resolveWrapper;
-				this._rejectCallback = rejectedWrapper;
-			}
-
-		});
 	}
 
 	finally(onFinally) {
